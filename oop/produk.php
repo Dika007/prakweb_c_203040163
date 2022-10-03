f852fec2938d48e4841c760b899ccc7d1a012c03
Dika <dsa799059@gmail.com>
2022-10-03 19:28:42 +0700

    Membuat Property dan Method

--- oop/produk.php
+++ oop/produk.php
@@ -1,12 +1,40 @@
 <?php
-// jualan produk
-// komik
-// game
+
 
 class Produk
 {
-}
-$produk1 =  new Produk();
-$produk2 =  new Produk();
+  public  $judul = "judul",
+    $penulis = "penulis",
+    $penerbit = "penerbit",
+    $harga = 0;
 
-var_dump($produk1);
+  public function getlabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+}
+// $produk1 =  new Produk();
+// $produk1->judul = "naruto";
+// var_dump($produk1);
+
+// $produk2 =  new Produk();
+// $produk2->judul = "Naskar";
+// $produk2->tambahproperty = "wadidaw";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 2000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckman";
+$produk4->penerbit = "Sony computer";
+$produk4->harga = 2500000;
+
+echo "Komik : " . $produk3->getlabel();
+
+echo "<br>";
+echo "Game : " . $produk4->getlabel();
