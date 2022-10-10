1f6574715c36bb9b6a933db76996e0198fce4616
Dika <dsa799059@gmail.com>
2022-10-10 23:10:15 +0700

    Membuat DataBase Wrapper

--- phpmvc/app/views/Mahasiswa/index.php
+++ phpmvc/app/views/Mahasiswa/index.php
@@ -2,15 +2,19 @@
   <div class="row">
     <div class="col-6">
       <h3>Daftar mahasiswa</h3>
+      <ul class="list-group">
+        <?php foreach ($data['mhs'] as $mhs) : ?>
+          <li class="list-group-item d-flex justify-content-between align-items-center ">
+            <?= $mhs['nama']; ?>
 
-      <?php foreach ($data['mhs'] as $mhs) : ?>
-        <ul>
-          <li><?= $mhs['nama']; ?></li>
-          <li><?= $mhs['nrp']; ?></li>
-          <li><?= $mhs['email']; ?></li>
-          <li><?= $mhs['jurusan']; ?></li>
-        </ul>
-      <?php endforeach; ?>
+            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary">detail</a>
+
+          </li>
+        <?php endforeach; ?>
+      </ul>
+
+
+
 
 
 
